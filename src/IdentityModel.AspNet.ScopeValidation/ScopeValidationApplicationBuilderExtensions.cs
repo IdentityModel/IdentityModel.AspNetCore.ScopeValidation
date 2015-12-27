d7866060afbbaf4784e64dee53261d52f04a55a7
Dominick Baier <dbaier@leastprivilege.com>
2015-12-27 22:28:41 +0100

    1st commit

---
+++ src/IdentityModel.AspNet.ScopeValidation/ScopeValidationApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using IdentityModel.AspNet.ScopeValidation;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class ScopeValidationApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AllowScopes(this IApplicationBuilder app, params string[] scopes)
+        {
+            var options = new ScopeValidationOptions
+            {
+                AllowedScopes = scopes
+            };
+
+            return app.AllowScopes(options);
+        }
+
+        public static IApplicationBuilder AllowScopes(this IApplicationBuilder app, ScopeValidationOptions options)
+        {
+            return app.UseMiddleware<ScopeValidationMiddleware>(options);
+        }
+    }
+}
---
+++ src/IdentityModel.AspNet.ScopeValidation/ScopeValidationOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace IdentityModel.AspNet.ScopeValidation
+{
+    public class ScopeValidationOptions
+    {
+        public string ScopeClaimType { get; set; } = "scope";
+        public IEnumerable<string> AllowedScopes { get; set; }
+    }
+}
