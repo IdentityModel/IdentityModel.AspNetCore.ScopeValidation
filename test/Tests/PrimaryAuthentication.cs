121157e86fe3380dde8d252e6b19d62c868d5697
Dominick Baier <dbaier@leastprivilege.com>
2016-01-03 18:50:49 +0100

    Added simple tests

---
+++ test/Tests/PrimaryAuthentication.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using IdentityModel;
+using Microsoft.AspNet.TestHost;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class PrimaryAuthentication
+    {
+        [Fact]
+        public async Task Anonymous_User_Should_Be_Allowed()
+        {
+            var principal = Principal.Anonymous;
+            var allowedScopes = new[] { "scope1", "scope2" };
+
+            var client = CreateClient(principal, allowedScopes);
+            var response = await client.GetAsync("/");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Authenticated_User_Missing_Scopes_Should_Be_Forbidden()
+        {
+            var principal = Principal.Create("custom",
+                new Claim("sub", "123"));
+            var allowedScopes = new[] { "scope1", "scope2" };
+
+            var client = CreateClient(principal, allowedScopes);
+            var response = await client.GetAsync("/");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Authenticated_User_Matching_Scope_Should_Be_Allowed()
+        {
+            var principal = Principal.Create("custom",
+                new Claim("sub", "123"),
+                new Claim("scope", "scope1"));
+            var allowedScopes = new[] { "scope1", "scope2" };
+
+            var client = CreateClient(principal, allowedScopes);
+            var response = await client.GetAsync("/");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        private HttpClient CreateClient(ClaimsPrincipal principal, IEnumerable<string> allowedScopes)
+        {
+            var startup = new PrimaryAuthenticationStartup(principal, allowedScopes);
+            var server = TestServer.Create(null, startup.Configure, startup.ConfigureServices);
+
+            return server.CreateClient();
+        }
+    }
+}
---
+++ test/Tests/PrimaryAuthenticationStartup.cs
@@ -0,0 +1,43 @@
+﻿using IdentityModel.AspNet.ScopeValidation;
+using Microsoft.AspNet.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class PrimaryAuthenticationStartup
+    {
+        private readonly IEnumerable<string> _allowedScopes;
+        private readonly ClaimsPrincipal _principal;
+
+        public PrimaryAuthenticationStartup(ClaimsPrincipal principal, IEnumerable<string> allowedScopes)
+        {
+            _principal = principal;
+            _allowedScopes = allowedScopes;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                ctx.User = _principal;
+                await next();
+            });
+
+            app.AllowScopes(new ScopeValidationOptions { AllowedScopes = _allowedScopes });
+
+            app.Run(ctx =>
+            {
+                ctx.Response.StatusCode = 200;
+                return Task.FromResult(0);
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        { }
+    }
+}
