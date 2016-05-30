0a9b3aff3a9d5c5e3ecec9dad4eee4b306ba3b2c
Dominick Baier <dbaier@leastprivilege.com>
2016-05-30 07:27:46 +0200

    rework project.json

--- src/IdentityModel.AspNetCore.ScopeValidation/Properties/AssemblyInfo.cs
+++ src/IdentityModel.AspNetCore.ScopeValidation/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -8,11 +7,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IdentityModel.AspNetCore.ScopeValidation")]
 [assembly: AssemblyTrademark("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("50dd725c-8e33-4209-8309-742c6bf1bc6a")]
